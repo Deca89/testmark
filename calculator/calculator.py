b6a701b1bd2e4a8ffb6d32b48d0b7b2575e68d0d
DennisA <dennis.apter@awacademy.training>
2022-12-12 11:30:47 +0200

    test data

---
+++ calculator/calculator.py
@@ -0,0 +1,12 @@
+
+
+
+
+def sum_numbers(number1, number2):
+    return number1 + number2
+
+def minus_numbers(number1, number2):
+    return number1 - number2
+
+def divide_numbers(number1, number2):
+    return number1 / number2
---
+++ tests/test_calculator.py
@@ -0,0 +1,72 @@
+import unittest
+import calculator.calculator as calculator
+
+class TestSumma(unittest.TestCase):
+    #test sum_numbers
+    def test_sum_numbers_returns_sum_of_integers(self):
+        result = calculator.sum_numbers(2, 3)
+        self.assertEqual(result, 5)
+
+    def test_sum_numbers_works_with_negative_numbers(self):
+        result = calculator.sum_numbers(-2, -3)
+        self.assertEqual(result, -5)
+
+    def test_sum_numbers_returns_sum_of_floats(self):
+        result = calculator.sum_numbers(2.4, 3.2)
+        self.assertEqual(result, 5.6)
+
+    def test_sum_numbers_returns_sum_of_integer_and_float(self):
+        result = calculator.sum_numbers(2, 3.2)
+        self.assertEqual(result, 5.2)
+
+    def test_sum_numbers_takes_rises_error_if_parameters_are_not_numbers(self):
+        with self.assertRaises(TypeError):
+            calculator.sum_numbers(1, "a")
+
+
+    #test minus_numbers
+    def test_minus_numbers_returns_minus_of_integers(self):
+        result = calculator.minus_numbers(2, 3)
+        self.assertEqual(result, -1)
+
+    def test_minus_numbers_works_with_negative_numbers(self):
+        result = calculator.minus_numbers(-2, -3)
+        self.assertEqual(result, 1)
+
+    def test_minus_numbers_returns_minus_of_floats(self):
+        result = calculator.minus_numbers(2.4, 3.2)
+        self.assertTrue((result - -0.8) < 0.0000000001)
+
+    def test_minus_numbers_returns_minus_of_integer_and_float(self):
+        result = calculator.minus_numbers(2, 3.2)
+        self.assertTrue((result - -1.2) < 0.0000000001)
+
+    def test_minus_numbers_takes_rises_error_if_parameters_are_not_numbers(self):
+        with self.assertRaises(TypeError):
+            calculator.minus_numbers(1, "a")
+    
+
+    #test divide_numbers
+    def test_divide_numbers_returns_divide_of_integers(self):
+        result = calculator.divide_numbers(3, 2)
+        self.assertEqual(result, 1.5)
+
+    def test_divide_numbers_works_with_negative_numbers(self):
+        result = calculator.divide_numbers(-3, -2)
+        self.assertEqual(result, 1.5)
+
+    def test_divide_numbers_returns_divide_of_floats(self):
+        result = calculator.divide_numbers(3.6, 0.6)
+        self.assertEqual(result, 6)
+
+    def test_divide_numbers_returns_divide_of_integer_and_float(self):
+        result = calculator.divide_numbers(3, 0.2)
+        self.assertEqual(result, 15)
+
+    def test_divide_numbers_takes_rises_error_if_parameters_are_not_numbers(self):
+        with self.assertRaises(TypeError):
+            calculator.divide_numbers(1, "a")
+
+
+
+if __name__ == '__main__': unittest.main()
